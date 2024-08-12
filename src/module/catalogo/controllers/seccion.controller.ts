e00b74fefe34a0ba7f75bf558fea3ef011a3f0fd
TaniaCruz09 <cruztania0909@gmail.com>
2024-08-11 21:18:40 -0600

    creando el CRUD de seccion en el controller

--- src/module/catalogo/controllers/seccion.controller.ts
+++ src/module/catalogo/controllers/seccion.controller.ts
@@ -1,32 +1,62 @@
-import { Controller, Get, Post } from "@nestjs/common";
-import { SeccionService } from "../services/seccion.service";
+import { Controller, Get, Post, Put, Delete, Body, Param, Query, ParseIntPipe } from '@nestjs/common';
+import { CreateSeccionDTO } from '../dtos/seccion.dto';
+import { SeccionService } from '../services/seccion.service';
 
 
 @Controller('seccion')
 export class SeccionController {
-    constructor(
-        private readonly seccionService: SeccionService
-    ){}
-    @Get('/')
-    async findAll(){
-        const seccion = await this.seccionService.findAll
-        const data = {
-            data: seccion,
-            message: 'ok',
-          };
-          return data;
-    }
+  constructor(private readonly seccionService: SeccionService) {}
 
-    @Get('/:id')
-    async findOne(){
-        const seccion = await this.seccionService.findOne
-        const data = {
-            data: seccion,
-            message: 'ok',
-          };
-          return data;
-    }
+  @Get('/')
+  async findAll() {
+    const seccion = await this.seccionService.findAll();
+    const data = {
+      data: seccion,
+      message: 'ok',
+    };
+    return data;
+  }
 
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    const seccion = await this.seccionService.findOne(id);
+    const data = {
+      data: seccion,
+      message: 'ok',
+    };
+    return data;
+  }
 
+  @Post('/')
+  async create(@Body() payload: CreateSeccionDTO){
+    const seccion = await this.seccionService.create(payload);
+    const data = {
+      data: seccion,
+      message: 'ok',
+    };
+    return data;
+  }
 
+  @Put('/:id')
+  async update(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body() payload: CreateSeccionDTO,
+  ){
+    const seccion = await this.seccionService.update(id, payload);
+    const data = {
+      data: seccion,
+      message: 'ok',
+    };
+    return data;
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    const seccion = await this.seccionService.delete(id);
+    const data = {
+      data: seccion,
+      message: 'ok',
+    };
+    return data;
+  }
 }

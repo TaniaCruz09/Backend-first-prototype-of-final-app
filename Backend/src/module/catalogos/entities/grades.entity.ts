47818aff3b73b5fbca035c281a078b37d1aa410d
Mitch-Luna <lunitamitch02@gmail.com>
2025-04-09 20:03:36 -0700

    creando un monolito

---
+++ Backend/src/module/catalogos/entities/grades.entity.ts
@@ -0,0 +1,22 @@
+import { Grupos } from '../../grupos/entities/grupos.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'catalogos', name: 'grades' })
+export class GradesEntity {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: 'int4',
+  })
+  id?: number;
+
+  @Column({
+    name: 'grades',
+    type: 'varchar',
+
+    nullable: true,
+  })
+  grades: string;
+
+  @OneToMany(() => Grupos, (grupo) => grupo.grado)
+  grupos?: Grupos[];
+}
